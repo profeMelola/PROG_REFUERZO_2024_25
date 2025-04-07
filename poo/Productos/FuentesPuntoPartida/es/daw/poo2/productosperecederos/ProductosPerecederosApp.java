a77217f4c993bf9baf67f65478e8c4e4a6abaa0b
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-04-07 19:13:53 +0200

    Add files via upload

---
+++ poo/Productos/FuentesPuntoPartida/es/daw/poo2/productosperecederos/ProductosPerecederosApp.java
@@ -0,0 +1,22 @@
+package es.daw.poo2.productosperecederos;
+
+import es.daw.poo2.productosperecederos.model.Perecedero;
+import es.daw.poo2.productosperecederos.model.Producto;
+import es.daw.poo2.productosperecederos.dao.ProductoDAO;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+
+/**
+ *
+ * @author melol
+ */
+public class ProductosPerecederosApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
